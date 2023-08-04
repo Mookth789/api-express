cf35bb56d4db4ae14a0c0a655ce0037d6bd6e4e2
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-04 17:33:28 +0700

    3 files modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const mongoose = require('mongoose');
 
 app.use(express.static('static'));
 
